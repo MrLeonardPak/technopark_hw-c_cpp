ea350cdb74001572804fff27b5bd8e0798f5ed5a
Leonard Pak <leopak2000@gmail.com>
2022-03-31 19:54:38 +0300

    Add FindClusterCentere_TEST

--- hw-2/tests/tests.cc
+++ hw-2/tests/tests.cc
@@ -19,18 +19,42 @@ extern "C" {
 
 TEST(KMEANS_TESTS, SquareEuclideanDistance_TEST) {
   size_t const ans_cnt = 3;
-  Point points_a[ans_cnt] = {{0, 0, 0}, {1, 2, 3}, {2, 2, 2}};
-  Point points_b[ans_cnt] = {{0, 0, 0}, {3, 2, 1}, {-2, -2, -2}};
-  int right_answers[ans_cnt] = {0, 8, 48};
+  Point const points_a[ans_cnt] = {{0, 0, 0}, {1, 2, 3}, {2, 2, 2}};
+  Point const points_b[ans_cnt] = {{0, 0, 0}, {3, 2, 1}, {-2, -2, -2}};
+  int const right_answers[ans_cnt] = {0, 8, 48};
   int answer = 0;
-
+  // Проверка на передачу NULL
+  EXPECT_EQ(FAILURE, SquareEuclideanDistance(NULL, &points_b[0], &answer));
+  EXPECT_EQ(FAILURE, SquareEuclideanDistance(&points_a[0], NULL, &answer));
+  EXPECT_EQ(FAILURE, SquareEuclideanDistance(&points_a[0], &points_b[0], NULL));
+  //Проверка на правильный вывод
   for (size_t i = 0; i < ans_cnt; ++i) {
     EXPECT_EQ(SUCCESS,
               SquareEuclideanDistance(&points_a[i], &points_b[i], &answer));
     EXPECT_EQ(right_answers[i], answer);
   }
+}
 
-  EXPECT_EQ(FAILURE, SquareEuclideanDistance(NULL, &points_b[0], &answer));
-  EXPECT_EQ(FAILURE, SquareEuclideanDistance(&points_a[0], NULL, &answer));
-  EXPECT_EQ(FAILURE, SquareEuclideanDistance(&points_a[0], &points_b[0], NULL));
+TEST(KMEANS_TESTS, FindClusterCentere_TEST) {
+  KMeans* kmeans = (KMeans*)calloc(1, sizeof(KMeans));
+  // Проверка на передачу NULL
+  EXPECT_EQ(FAILURE, FindClusterCenter(NULL, 0));
+  EXPECT_EQ(FAILURE, FindClusterCenter(kmeans, kmeans->clusters_cnt + 1));
+  //Проверка на правильный вывод
+  kmeans->clusters_cnt = 1;
+  kmeans->clusters = (Point*)malloc(kmeans->clusters_cnt * sizeof(Point));
+  kmeans->points_cnt = 4;
+  kmeans->points =
+      (PointInCluster*)malloc(sizeof(PointInCluster) * kmeans->points_cnt);
+  Point const right_answers = {2, 2, 2};
+  kmeans->points[0] = {1, 1, 1, 0};
+  kmeans->points[1] = {3, 3, 3, 0};
+  kmeans->points[2] = {1, 3, 1, 0};
+  kmeans->points[3] = {3, 1, 3, 0};
+  EXPECT_EQ(SUCCESS, FindClusterCenter(kmeans, 0));
+  EXPECT_EQ(right_answers.x, kmeans->clusters[0].x);
+  EXPECT_EQ(right_answers.y, kmeans->clusters[0].y);
+  EXPECT_EQ(right_answers.z, kmeans->clusters[0].z);
+
+  free(kmeans);
 }
