3b3f178220955fc031549e6f927df3adc6bffb73
Leonard Pak <leopak2000@gmail.com>
2022-03-31 19:29:54 +0300

    Update test

--- hw-2/tests/tests.cc
+++ hw-2/tests/tests.cc
@@ -18,17 +18,17 @@ extern "C" {
 }
 
 TEST(KMEANS_TESTS, SquareEuclideanDistance_TEST) {
-  Point points_a[] = {{0, 0, 0}, {1, 2, 3}};
-  Point points_b[] = {{0, 0, 0}, {3, 2, 1}};
-  int right_answers[] = {0, 8};
+  size_t const ans_cnt = 3;
+  Point points_a[ans_cnt] = {{0, 0, 0}, {1, 2, 3}, {2, 2, 2}};
+  Point points_b[ans_cnt] = {{0, 0, 0}, {3, 2, 1}, {-2, -2, -2}};
+  int right_answers[ans_cnt] = {0, 8, 48};
   int answer = 0;
 
-  EXPECT_EQ(SUCCESS,
-            SquareEuclideanDistance(&points_a[0], &points_b[0], &answer));
-  EXPECT_EQ(right_answers[0], answer);
-  EXPECT_EQ(SUCCESS,
-            SquareEuclideanDistance(&points_a[1], &points_b[1], &answer));
-  EXPECT_EQ(right_answers[1], answer);
+  for (size_t i = 0; i < ans_cnt; ++i) {
+    EXPECT_EQ(SUCCESS,
+              SquareEuclideanDistance(&points_a[i], &points_b[i], &answer));
+    EXPECT_EQ(right_answers[i], answer);
+  }
 
   EXPECT_EQ(FAILURE, SquareEuclideanDistance(NULL, &points_b[0], &answer));
   EXPECT_EQ(FAILURE, SquareEuclideanDistance(&points_a[0], NULL, &answer));
