939c6b402ea3f21b38be806f39821bf4a70cd1e3
Leonard Pak <leopak2000@gmail.com>
2022-04-02 01:16:46 +0300

    Shredding tests for serial and multi

--- hw-2/tests/kmeans_tests.cc
+++ hw-2/tests/kmeans_tests.cc
@@ -1,15 +1,7 @@
 /**
- * @file test.cc
- * @author your name (you@domain.com)
- * @brief
  * lcov -t "tests/tests_kmeans" -o coverage.info -c -d kmeans_libs/
  * genhtml -o report coverage.info
  * cd report && python3 -m http.server 8000
- * @version 0.1
- * @date 2022-03-16
- *
- * @copyright Copyright (c) 2022
- *
  */
 #include "gtest/gtest.h"
 
@@ -83,21 +75,4 @@ TEST(KMEANS_TESTS, ClusterSort_TEST) {
   // Освобождение памяти
   free(kmeans->points);
   free(kmeans);
-}
-
-TEST(KMEANS_TESTS, MAIN_TEST) {
-  KMeans* kmeans = NULL;
-  int status = system("data/data 1 /tmp/data.bin");
-  ASSERT_EQ(status, 0);
-  // Проверка на передачу NULL
-  EXPECT_EQ(FAILURE, CreatPoints(NULL, "/tmp/data.bin"));
-  EXPECT_EQ(FAILURE, CreatPoints(&kmeans, NULL));
-  EXPECT_EQ(FAILURE, StartAlgorithm(NULL));
-  EXPECT_EQ(FAILURE, PrintClusters(NULL));
-  EXPECT_EQ(FAILURE, DeletePoints(NULL));
-  // Проверка на полную работу
-  ASSERT_EQ(SUCCESS, CreatPoints(&kmeans, "/tmp/data.bin"));
-  EXPECT_EQ(SUCCESS, StartAlgorithm(kmeans));
-  EXPECT_EQ(SUCCESS, PrintClusters(kmeans));
-  EXPECT_EQ(SUCCESS, DeletePoints(&kmeans));
 }
---
+++ hw-2/tests/serial_tests.cc
@@ -0,0 +1,29 @@
+/**
+ * lcov -t "tests/tests_kmeans" -o coverage.info -c -d kmeans_libs/
+ * genhtml -o report coverage.info
+ * cd report && python3 -m http.server 8000
+ */
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "kmeans.h"
+}
+
+TEST(KMEANS_TESTS, MAIN_TEST) {
+  KMeans* kmeans = NULL;
+  int status = system("data/data 1 /tmp/data.bin");
+  ASSERT_EQ(status, 0);
+  // Проверка на передачу NULL
+  EXPECT_EQ(FAILURE, CreatPoints(NULL, "/tmp/data.bin"));
+  EXPECT_EQ(FAILURE, CreatPoints(&kmeans, NULL));
+  EXPECT_EQ(FAILURE, StartAlgorithm(NULL));
+  EXPECT_EQ(FAILURE, PrintClusters(NULL));
+  EXPECT_EQ(FAILURE, DeletePoints(NULL));
+  // Проверка на неправильный файл
+  EXPECT_EQ(FAILURE, CreatPoints(&kmeans, "no_file.bin"));
+  // Проверка на полную работу
+  ASSERT_EQ(SUCCESS, CreatPoints(&kmeans, "/tmp/data.bin"));
+  EXPECT_EQ(SUCCESS, StartAlgorithm(kmeans));
+  EXPECT_EQ(SUCCESS, PrintClusters(kmeans));
+  EXPECT_EQ(SUCCESS, DeletePoints(&kmeans));
+}
